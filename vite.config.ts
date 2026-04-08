0418209c5a59c7b5d6d19b02a47de1fa1b6dc38c
Krishna <gopavarapukrishnakarthik@gmail.com>
2026-04-08 05:58:35 +0530

    delpoyment vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/aquadrop/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
